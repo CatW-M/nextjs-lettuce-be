d185f60e691eb21669c3be09a624c68b19c04820
Catherine Wheeler-Mushlin <107967088+CatW-M@users.noreply.github.com>
2023-03-02 01:25:53 -0700

    Passed prop to ProductItems component and rendered

--- components/ProductItem.js
+++ components/ProductItem.js
@@ -5,18 +5,18 @@ import React from 'react';
 export default function ProductItem({ product }) {
   return (
     <div className="card">
-      <Link href={`/product/${product.slug}`}>
-        <Image
-          src={product.image}
-          alt={product.name}
-          className="rounded shadow"
-        ></Image>
-      </Link>
       <div className="flex flex-col items-center justify-center p-5">
         <Link href={`/product/${product.slug}`}>
-          <h2 className="text-lg">{product.name}</h2>
+          <h2 className="text-lg" style={{ textTransform: 'capitalize' }}>
+            {product.name}
+          </h2>
         </Link>
-        <p className="mb-2">Suggested Storage:{product.storage}</p>
+        <p className="mb-2" style={{ textTransform: 'capitalize' }}>
+          Suggested Storage:
+        </p>
+        <p className="mb-2" style={{ textTransform: 'capitalize' }}>
+          {product.storage}
+        </p>
         <button className="primary-button" type="button">
           Add to Inventory
         </button>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
+import ProductItem from '@/components/ProductItem';
+import data from '@/utils/data';
 import RootLayout from './layout';
 
 export default function Home() {
-  return <RootLayout title="Home Page">Home Page</RootLayout>;
+  return (
+    <RootLayout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </RootLayout>
+  );
 }
