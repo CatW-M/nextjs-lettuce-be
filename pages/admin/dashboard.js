79d7a3c239cd8a53370df51ffbe765d2cf42e67c
Catherine Wheeler-Mushlin <107967088+CatW-M@users.noreply.github.com>
2023-03-03 18:29:10 -0700

    Fix: Auth and added dummy page for admin dashboard

---
+++ pages/admin/dashboard.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function AdminDashboardScreen() {
+  return <div>Welcome to the Admin Screen - A Work in Progress</div>;
+}
+
+AdminDashboardScreen.auth = { adminOnly: true };
